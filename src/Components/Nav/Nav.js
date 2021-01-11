2adb110ef296c2699877a183a0d225771f38e455
이 필 제 <xxpiiiide@gmail.com>
2021-01-11 21:46:47 +0900

    Add: NavBar 스켈레톤완성

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import CategoryMenu from './CategoryMenu/CategoryMenu';
-import Login from './Login/Login';
+// import CategoryMenu from './CategoryMenu/CategoryMenu';
+// import Login from './Login/Login';
+import './Nav.scss';
 
 
 class Nav extends Component {
   render () {
     return (
-      <div> Nav Bar
-        <CategoryMenu />
-        <Login />
-      </div>
+      <nav className="Nav">  
+        {/* <CategoryMenu />
+        <Login /> */}
+        <div>
+          <ul>
+            <li>제품보기</li>
+            <li>읽기</li>
+            <li>스토어</li>
+            <li>검색</li>
+          </ul>
+        </div>
+        <div>
+
+
+        </div>
+        <div>
+          <ul>
+            <li>로그인</li>
+            <li>카트</li>
+          </ul>
+        </div>
+
+      </nav>
     );
   }
 
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Nav from '../../Components/Nav/Nav';
-
+import './Main.scss';
 
 class Main extends Component {
   render () {
     return (
-      <div> Main Menu
+      <div> 
         <Nav />
+
       </div>
 
     );
