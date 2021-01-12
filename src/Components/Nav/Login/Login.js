b15a4cd93b697df395e54aa949d57a42fa1f6357
이 필 제 <xxpiiiide@gmail.com>
2021-01-12 16:53:02 +0900

    Add: Login.js 세팅

--- src/Components/Nav/Login/Login.js
+++ src/Components/Nav/Login/Login.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import './Login.scss';
 
 
 class Login extends Component {
   render () {
     return (
-      <div> Login Menu</div>
+      <div> 
+        <div class="modal_login">
+            <h2>안녕하세요</h2>
+            <h4>유효한 이메일 주소를 입력하세요</h4>
+            <input type="text" placeholder="이메일 주소" />
+            <button>계속</button>
+            <span class='modal-close'>X</span>
+        </div>
+        
+      </div>
     );
   }
 
