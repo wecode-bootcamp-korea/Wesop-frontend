02ce1cc3fdea2e40d336cbe810309d3f217ecd9c
이 필 제 <xxpiiiide@gmail.com>
2021-01-12 22:15:04 +0900

    Add: Nav Modal 구현

--- src/Components/Nav/Login/Login.js
+++ src/Components/Nav/Login/Login.js
@@ -4,17 +4,17 @@ import './Login.scss';
 class Login extends Component {
   render () {
     return (
-      <div>
-        <div class="Login_modal"> 
+      <div className="Login">
+        <div className="LoginModal"> 
          <h1>안녕하세요.</h1>
          <h3>유효한 이메일 주소를 입력하세요.</h3>
          <input type='text' placeholder='이메일 주소' />
          <button>계속</button>
-         <span class="closebutton">X</span>
+         <span className="closeButton">X</span>
 
 
         </div>
-      </div>
+      </div>  
     );
   }
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,38 +1,65 @@
 import React, { Component } from 'react';
 // import CategoryMenu from './CategoryMenu/CategoryMenu';
-// import Login from './Login/Login';
+import Login from './Login/Login';
+// import NavList from './NavList/NavList'; // 성현님 컴포넌트 
 import './Nav.scss';
 
-
 class Nav extends Component {
+  constructor () {
+    super();
+    this.state= {
+      isLoginShown: false,
+      isNavListShown: false,
+    } 
+  }
+  
+  handleLoginModal = () => {
+    this.setState({
+      isLoginShown: !this.state.isLoginShown,
+    })
+  }
+
+  handleNavListModal = () => {
+    this.setState({
+      isNavListShown: !this.state.isNavListShown,
+    })
+  }
+
+  
   render () {
+    const { isLoginShown, isNavListShown } = this.state;
     return (
       <nav className="Nav">  
-        {/* <CategoryMenu />
-        <Login /> */}
+    
         <div>
           <ul>
-            <li>제품보기</li>
-            <li>읽기</li>
-            <li>스토어</li>
-            <li>검색</li>
+            <span onClick={this.handleNavListModal} >제품보기</span>
+            
+            <span>읽기</span>
+            <span>스토어</span>
+            <span>검색</span>
           </ul>
         </div>
         <div>
 
-
         </div>
         <div>
           <ul>
-            <li>로그인</li>
-            <li>카트</li>
+            <span onClick={this.handleLoginModal}>로그인</span>
+            <span>카트</span>
           </ul>
-        </div>
+         </div>
 
+         <div className={isLoginShown ?"show" : "hide"}>
+            <Login />
+         </div>
+         
+         <div className={isNavListShown ? "show" : "hide"}>
+            {/* <NavList /> // 컴포넌트 import 하기 전  */}
+         </div>
       </nav>
     );
   }
-
 }
 
 export default Nav;
