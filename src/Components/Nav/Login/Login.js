0f83f088fa77c80ddb81e64404f0619ee7ae8c8f
이 필 제 <xxpiiiide@gmail.com>
2021-01-12 13:18:50 +0900

    Add: NavBar 스켈레톤완성&common.scss

--- src/Components/Nav/Login/Login.js
+++ src/Components/Nav/Login/Login.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-
+import './Login.scss';
 
 class Login extends Component {
   render () {
     return (
-      <div> Login Menu</div>
+      <div>
+        <div class="Login_modal"> 
+         <h1>안녕하세요.</h1>
+         <h3>유효한 이메일 주소를 입력하세요.</h3>
+         <input type='text' placeholder='이메일 주소' />
+         <button>계속</button>
+         <span class="closebutton">X</span>
+
+
+        </div>
+      </div>
     );
   }
-
 }
 
 export default Login;
