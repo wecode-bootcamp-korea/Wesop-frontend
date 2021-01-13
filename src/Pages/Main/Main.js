c4961c54632f6303d0cca457fe6400c03ea877ae
unknown <dev.hannahk@gmail.com>
2021-01-13 20:51:55 +0900

    Add: Mock Data 리뷰 수정 사항 반영 및 메인페이지 레이아웃 틀 완성

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -21,20 +21,60 @@ class Main extends Component {
               <div className="mainSection-1-leftSide-innerWrapper-content">
                 <p>세 가지 아로마 캔들이 이솝의 홈 레인지를 통해 새롭게 선보입니다. 고대 천문학자의 이름을 붙인 캔들은 고유한 매혹적인 향기를 전합니다.</p>
                 <div className="mainSection-1-leftSide-innerWrapper-content-button">
-                  <span><a href="#">레인지 살펴보기</a></span>
+                  <span>레인지 살펴보기</span>
                 </div>
               </div>
             </div>
           </div>
           <div className="mainSection-1-rightSide">
-            <img alt="이미지" src="https://www.aesop.com/u1nb1km7t5q7/2lcsVIwzcf7YupCndczysa/4e8755959b13dac77f0749dab5a89ba4/Aesop_Poured_Candles_2020_Hybris_Homepage_Secondary_Split_02_960x540px.gif" />
+            <img alt="new_product_promotion_img" src="https://www.aesop.com/u1nb1km7t5q7/2lcsVIwzcf7YupCndczysa/4e8755959b13dac77f0749dab5a89ba4/Aesop_Poured_Candles_2020_Hybris_Homepage_Secondary_Split_02_960x540px.gif" />
           </div>
         </section>      
         <MainSlider />
-        <section className="mainSection-3">씨드스킨케어</section>
-        <section className="mainSection-carouselSlider-2">씨드_슬라이더 이미지</section>
-        <section className="mainSection-5">스토어 로케이터</section>
-        <section className="mainSection-6">Quote</section>  
+        <div className="mainSection-3">
+          <div className="mainSection-3-leftside">
+            <div className="mainSection-3-leftSide-innerWrapper">
+              <header>
+                <h2>파슬리 씨드 스킨 케어</h2>
+                <h1>파슬리 씨드 스킨 케어</h1>
+              </header>
+              <div className="mainSection-3-leftSide-innerWrapper-content">
+                <p>각종 오염, 난방기의 사용, 도시 생활에서 오는 스트레스로 인해 피부는 주변 환경에 민감하게 반응할 수 있습니다. 1) 항산화 성분을 풍부하게 함유한 파슬리 씨드 제품은 피부를 탄탄하게 강화시켜 줍니다.</p>
+                <div className="mainSection-3-leftSide-innerWrapper-content-button">
+                  <span>서늘한 환경에서의 피부 관리법</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mainSection-3-rightSide">
+            <img alt="seed_skincare_img" width="1110px" src="https://www.aesop.com/u1nb1km7t5q7/5Uchgf3EITLPDvmZgf3Cj2/2c648d927c44ac2c844e6e5cce10cba9/Aesop-Parsley-Seed-Performance-Campaign-2020-Hybris-Homepage-Secondary-3-Mid-Desktop-1690x1150px.jpg" />
+          </div>
+        </div>
+        <MainSlider />
+        <div className="mainSection-3">
+          <div className="mainSection-3-leftside">
+            <div className="mainSection-3-leftSide-innerWrapper">
+              <header>  
+                <h1>스토어 로케이터</h1>
+              </header>
+              <div className="mainSection-3-leftSide-innerWrapper-content">
+                <p>매장에서는 이솝 컨설턴트가 고객 여러분을 따뜻하게 맞이하고 기프트 구매를 위한 맞춤형 컨설팅을 제공해드립니다.</p>
+                <div className="mainSection-3-leftSide-innerWrapper-content-button">
+                  <span>가까운 스토어 찾기</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mainSection-3-rightSide">
+            <img alt="seed_skincare_img" width="1110px" src="https://www.aesop.com/u1nb1km7t5q7/78lSsJHoVvtMYVyynstu3e/cc94bae54e56d636857ab94f2017b0be/Aesop-KR-Lotte-World-Tower-Mall-Hero-Bleed-Desktop-2880x1620px.jpg" />
+          </div>
+        </div>
+        <section className="mainSection-6">
+          <div className="mainSection-6-quote">
+            <div className="mainSection-6-quote-title">‘It is high time to rekindle the stars.’</div>
+            <div className="mainSection-6-quote-writer">Guillaume Apollinaire</div>
+          </div>
+        </section>  
       </div>
 
     );
--- src/Pages/Main/MainSlider.js
+++ src/Pages/Main/MainSlider.js
@@ -1,54 +1,69 @@
 import React, { Component } from 'react';
-import Slider from "react-slick";
+//import Slider from "react-slick";
 import './MainSlider.scss'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+//import "slick-carousel/slick/slick.css";
+//import "slick-carousel/slick/slick-theme.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 class MainSlider extends Component {
   constructor() {
     super();
     this.state = {
-        mainSliderData: []
+        mainSliderProducts: []
     }
   }
   
   componentDidMount = () => {
-      fetch('data/MainSliderData.json')
-        .then(res => res.json())
-        .then(data => {
-            this.setState({
-                mainSliderData: data.mainSliderData
-            })
-        })
-  }
-
-  render() {
-    const settings = {
-      className: "slider variable-width",
-      dots: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      variableWidth: true,
-      draggable: true,
-      infinite: false
+    fetch('data/MainSliderProducts.json')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          mainSliderProducts: data
+          })
+        } 
+      )
     }
 
+  render() {
+    const { mainSliderProducts } = this.state
+
+    const responsive = {
+      desktop: {
+        breakpoint: { max: 3000, min: 1500 },
+        items: 4,
+        slidesToSlide: 1 
+      },
+      tablet: {
+        breakpoint: { max: 1500, min: 700 },
+        items: 2,
+        slidesToSlide: 1
+      },
+      mobile: {
+        breakpoint: { max: 700, min: 0 },
+        items: 1,
+        slidesToSlide: 1
+      }
+    };
+
     return (
-        <section className="mainSection-carouselSlider-1">
-        <Slider {...settings}>
-            {this.state.mainSliderData && 
-             this.state.mainSliderData.map( element => {
+        <section className="mainSection-Slider">
+        <Carousel responsive={responsive} >
+            {mainSliderProducts && 
+             mainSliderProducts.map( item => {
                  return (
-                    <div key={element.id}>
-                    <img alt={element.productName} src={element.url} height="400px"/>
-                    <span>
-                        <p>{element.productName}</p>
-                        <p>{element.productDescription}</p>
-                    </span>
-                    </div>
-                 )
-             })}
-        </Slider>
+                  <div  key={item.id} className="mainSection-Slider-items-list">
+                    <div className="mainSection-Slider-items">
+                      <img alt={item.name} src={item.url} height="400px"/>
+                      <span className="mainSection-Slider-items-description">
+                          <span>{item.name}</span>
+                          <span>{item.description}</span>
+                      </span>
+                    </div>   
+                  </div>
+                  )
+              })}
+        </Carousel>
         </section>
     );
   }
