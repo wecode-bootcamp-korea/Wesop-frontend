551b534e7e70566089722243bf73a499375e1a67
unknown <dev.hannahk@gmail.com>
2021-01-18 00:22:44 +0900

    Add: 백엔드 서버와의 통신 성공 및 마우스 오버시에만 카트에 추가 버튼 보이도록 수정

--- src/Pages/ProductList/Components/ProductListContent.js
+++ src/Pages/ProductList/Components/ProductListContent.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+
 import './ProductListContent.scss';
 
 class ProductListContent extends Component {
+  constructor() {
+    super();
+    this.state = {
+        Products: [],
+        isHovering: false
+    }
+  }
+  
+  componentDidMount = () => {
+    fetch('http://192.168.200.114:8000/products')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          Products: res.products
+          })
+        } 
+      )
+    }
+
+  handleMouseHover = () => {
+    console.log("마우스오버");
+    this.setState(this.toggleHoverState);
+  }
+
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering,
+    };
+  }
+
   render () {
+    console.log(this.state.Products);
     return (
       <div className="ProductListContent">
         <div className="ProductListContent-intro">
@@ -13,99 +45,50 @@ class ProductListContent extends Component {
           각질, 불필요한 유분 그리고 기타 잔여물을 말끔히 씻어내어 피부를 깨끗하게 하는 것은 인텔리전트 스킨케어의 기초입니다.
           </p>
         </div>
-        <div className="ProductListContent-item">
-          <div className="ProductListContent-detail">
-            <div className="ProductListContent-detail-image">
-              <img width="400px"src="https://www.aesop.com/medias/Aesop-Skin-Remove-60mL-large.png?context=bWFzdGVyfGltYWdlc3wzNTg0NDJ8aW1hZ2UvcG5nfGltYWdlcy9oMTIvaDQxLzg4MDUwNzIxNDIzNjYucG5nfDM2ODViMzA0ZWU1NGU0MzBkOGZjMGZlNjlhMTU2YjE2ZTQ0ZTY2NjY5MjBhZDRiN2NhNDU4NzgyYmE2NGNkMGE" alt="remover" />
+        {this.state.Products &&
+        this.state.Products.map(item => {
+          const image = Object.values(item.media)[0];
+          const regex = /::/;
+          const idx = image.match(regex).index;
+          return (
+            <div 
+              onMouseEnter={this.handleMouseHover}
+              onMouseLeave={this.handleMouseHover}
+              className="ProductListContent-item">
+            <div className="ProductListContent-detail">
+              <div className="ProductListContent-detail-image">
+                <img width="400px"src={image.slice(0, idx)} alt={item.name} />
+              </div>
+              <div className="ProductListContent-detail-info">
+                <div className="ProductListContent-detail-info-name">
+                  <span>{item.name}</span>
+                </div>
+                <div className="ProductListContent-detail-info-price">
+                  <span>{item.size}</span>
+                  <span>/</span>
+                  <span>₩{Math.floor(item.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+                </div>
+              </div>
+              <div className="ProductListContent-detail-type">
+                <div className="ProductListContent-detail-type-kindof">
+                  <span>피부 타입</span>
+                  <span>{Object.values(item.skin_types).join(', ')}</span>
+                </div>
+                <div className="ProductListContent-detail-type-texture">
+                  <span>사용감</span>
+                  <span>{Object.values(item.feels).join(', ')}</span>
+                </div>
+              </div>
             </div>
-            <div className="ProductListContent-detail-info">
-              <div className="ProductListContent-detail-info-name">
-                <span>아이 리무버</span>
-              </div>
-              <div className="ProductListContent-detail-info-price">
-                <span>60ml</span>
-                <span>/</span>
-                <span>30,000</span>
-              </div>
-            </div>
-            <div className="ProductListContent-detail-type">
-              <div className="ProductListContent-detail-type-kindof">
-                <span>피부 타입</span>
-                <span>모든 피부, 메이크업을 한 피부</span>
-              </div>
-              <div className="ProductListContent-detail-type-texture">
-                <span>사용감</span>
-                <span>진정된, 생기있는</span>
-              </div>
-            </div>
+            { this.state.isHovering &&
+            <div className="ProductListContent-addcart">
+              <span>카트에 추가</span><span>-</span><span>₩{Math.floor(item.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+            </div> }
           </div>
-          <div className="ProductListContent-addcart">
-            <span>카트에 추가</span><span>-</span><span>₩30,000</span>
-          </div>
-        </div>
-        <div className="ProductListContent-item">
-          <div className="ProductListContent-detail">
-            <div className="ProductListContent-detail-image">
-              <img width="400px"src="https://www.aesop.com/medias/Aesop-Skin-Remove-60mL-large.png?context=bWFzdGVyfGltYWdlc3wzNTg0NDJ8aW1hZ2UvcG5nfGltYWdlcy9oMTIvaDQxLzg4MDUwNzIxNDIzNjYucG5nfDM2ODViMzA0ZWU1NGU0MzBkOGZjMGZlNjlhMTU2YjE2ZTQ0ZTY2NjY5MjBhZDRiN2NhNDU4NzgyYmE2NGNkMGE" alt="remover" />
-            </div>
-            <div className="ProductListContent-detail-info">
-              <div className="ProductListContent-detail-info-name">
-                <span>아이 리무버</span>
-              </div>
-              <div className="ProductListContent-detail-info-price">
-                <span>60ml</span>
-                <span>/</span>
-                <span>30,000</span>
-              </div>
-            </div>
-            <div className="ProductListContent-detail-type">
-              <div className="ProductListContent-detail-type-kindof">
-                <span>피부 타입</span>
-                <span>모든 피부, 메이크업을 한 피부</span>
-              </div>
-              <div className="ProductListContent-detail-type-texture">
-                <span>사용감</span>
-                <span>진정된, 생기있는</span>
-              </div>
-            </div>
-          </div>
-          <div className="ProductListContent-addcart">
-            <span>카트에 추가</span><span>-</span><span>₩30,000</span>
-          </div>
-        </div>
-        <div className="ProductListContent-item">
-          <div className="ProductListContent-detail">
-            <div className="ProductListContent-detail-image">
-              <img width="400px"src="https://www.aesop.com/medias/Aesop-Skin-Remove-60mL-large.png?context=bWFzdGVyfGltYWdlc3wzNTg0NDJ8aW1hZ2UvcG5nfGltYWdlcy9oMTIvaDQxLzg4MDUwNzIxNDIzNjYucG5nfDM2ODViMzA0ZWU1NGU0MzBkOGZjMGZlNjlhMTU2YjE2ZTQ0ZTY2NjY5MjBhZDRiN2NhNDU4NzgyYmE2NGNkMGE" alt="remover" />
-            </div>
-            <div className="ProductListContent-detail-info">
-              <div className="ProductListContent-detail-info-name">
-                <span>아이 리무버</span>
-              </div>
-              <div className="ProductListContent-detail-info-price">
-                <span>60ml</span>
-                <span>/</span>
-                <span>30,000</span>
-              </div>
-            </div>
-            <div className="ProductListContent-detail-type">
-              <div className="ProductListContent-detail-type-kindof">
-                <span>피부 타입</span>
-                <span>모든 피부, 메이크업을 한 피부</span>
-              </div>
-              <div className="ProductListContent-detail-type-texture">
-                <span>사용감</span>
-                <span>진정된, 생기있는</span>
-              </div>
-            </div>
-          </div>
-          <div className="ProductListContent-addcart">
-            <span>카트에 추가</span><span>-</span><span>₩30,000</span>
-          </div>
-        </div>
-       
-      </div>
-        )
+          )
+        })}
+     </div>
+    )
   }
 }
 
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const SERVER = "http://192.168.200.114:8000";
+
+export const PRODUCT_API = `${SERVER}/products`;
