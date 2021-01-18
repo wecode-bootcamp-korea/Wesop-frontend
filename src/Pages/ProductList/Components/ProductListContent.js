0bd78bff6b59bea2d90f57cc54f3d9bb976d8443
unknown <dev.hannahk@gmail.com>
2021-01-18 17:01:22 +0900

    Modify: 리뷰 반영하여 수정

--- src/Pages/ProductList/Components/ProductListContent.js
+++ src/Pages/ProductList/Components/ProductListContent.js
@@ -1,40 +1,36 @@
 import React, { Component } from 'react';
-
+import { PRODUCT_API } from '../../../config';
 import './ProductListContent.scss';
+
+const IMAGE_DIVIDER_REGEXP = /::/;
+const NUMBER_FORMAT_REGEXP = /\B(?=(\d{3})+(?!\d))/g;
 
 class ProductListContent extends Component {
   constructor() {
     super();
     this.state = {
-        Products: [],
+        products: [],
         isHovering: false
     }
   }
   
   componentDidMount = () => {
-    fetch('http://192.168.200.114:8000/products')
+    fetch( PRODUCT_API )
       .then(res => res.json())
       .then(res => {
         this.setState({
-          Products: res.products
+          products: res.products
           })
         } 
       )
     }
 
   handleMouseHover = () => {
-    console.log("마우스오버");
-    this.setState(this.toggleHoverState);
+    this.setState((prevState) => ({ isHovering: !prevState.isHovering}));
   }
-
-  toggleHoverState(state) {
-    return {
-      isHovering: !state.isHovering,
-    };
-  }
-
+  
   render () {
-    console.log(this.state.Products);
+    const { products } = this.state;
     return (
       <div className="ProductListContent">
         <div className="ProductListContent-intro">
@@ -45,11 +41,10 @@ class ProductListContent extends Component {
           각질, 불필요한 유분 그리고 기타 잔여물을 말끔히 씻어내어 피부를 깨끗하게 하는 것은 인텔리전트 스킨케어의 기초입니다.
           </p>
         </div>
-        {this.state.Products &&
-        this.state.Products.map(item => {
-          const image = Object.values(item.media)[0];
-          const regex = /::/;
-          const idx = image.match(regex).index;
+        {products &&
+        products.map(product => {
+          const [ image ] = Object.values(product.media);
+          const { index } = image.match(IMAGE_DIVIDER_REGEXP);
           return (
             <div 
               onMouseEnter={this.handleMouseHover}
@@ -57,32 +52,32 @@ class ProductListContent extends Component {
               className="ProductListContent-item">
             <div className="ProductListContent-detail">
               <div className="ProductListContent-detail-image">
-                <img width="400px"src={image.slice(0, idx)} alt={item.name} />
+                <img width="400px"src={image.slice(0, index)} alt={product.name} />
               </div>
               <div className="ProductListContent-detail-info">
                 <div className="ProductListContent-detail-info-name">
-                  <span>{item.name}</span>
+                  <span>{product.name}</span>
                 </div>
                 <div className="ProductListContent-detail-info-price">
-                  <span>{item.size}</span>
+                  <span>{product.size}</span>
                   <span>/</span>
-                  <span>₩{Math.floor(item.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+                  <span>₩{Math.floor(product.price).toString().replace(NUMBER_FORMAT_REGEXP, ",")}</span>
                 </div>
               </div>
               <div className="ProductListContent-detail-type">
-                <div className="ProductListContent-detail-type-kindof">
+                <div className="ProductListContent-detail-type-skintype">
                   <span>피부 타입</span>
-                  <span>{Object.values(item.skin_types).join(', ')}</span>
+                  <span>{Object.values(product.skin_types).join(', ')}</span>
                 </div>
                 <div className="ProductListContent-detail-type-texture">
                   <span>사용감</span>
-                  <span>{Object.values(item.feels).join(', ')}</span>
+                  <span>{Object.values(product.feels).join(', ')}</span>
                 </div>
               </div>
             </div>
             { this.state.isHovering &&
             <div className="ProductListContent-addcart">
-              <span>카트에 추가</span><span>-</span><span>₩{Math.floor(item.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+              <span>카트에 추가</span><span>-</span><span>₩{Math.floor(product.price).toString().replace(NUMBER_FORMAT_REGEXP, ",")}</span>
             </div> }
           </div>
           )
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const SERVER = "http://192.168.200.114:8000";
+export const SERVER = "http://10.58.5.193:8000";
 
 export const PRODUCT_API = `${SERVER}/products`;
